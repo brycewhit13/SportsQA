7f165c33cc218e9f43243d2825dc4f431c9f6ff0
Bryce Whitney <brycewhit13@gmail.com>
2023-03-24 16:38:06 -0400

    handle data loading in setup.py

--- scripts/constants.py
+++ scripts/constants.py
@@ -7,10 +7,11 @@
 ##########################################################################
 
 # Imports
+import os
 from nltk.corpus import stopwords
 
 # CONSTANTS
-RAW_DATA_FOLDER_PATH = "../data/raw_data"
-TEXT_DATA_FOLDER_PATH = "../data/text_data"
-PROCESSED_DATA_FOLDER_PATH = "../data/processed_data"
+RAW_DATA_FOLDER_PATH = os.path.join("data", "raw_data", "raw_data")
+TEXT_DATA_FOLDER_PATH = os.path.join("data", "text_data")
+PROCESSED_DATA_FOLDER_PATH = os.path.join("data", "processed_data")
 STOPWORDS_SET = set(stopwords.words("english"))
--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
+#################################################################
+# Filename: setup.py                                            #
+# Author: Bryce Whitney                                         #
+# Last Edit: 3/24/2023                                          #
+#                                                               #
+# Description: Performs all the necessary setup for the project #
+#################################################################
+
 # Imports
+import os
 import nltk
+from scripts.constants import TEXT_DATA_FOLDER_PATH, PROCESSED_DATA_FOLDER_PATH
+# Install the necessary libraries
+print("Installing the necessary libraries...")
+os.system("pip install -r requirements.txt")
 
 # Download the necessary nltk packages
 print("Downloading nltk punkt and stopwords packages...")
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
+
+# Load the text data if the foler is empty
+if len(os.listdir(TEXT_DATA_FOLDER_PATH)) == 0:
+    print("Loading the text data...")
+    os.system("python scripts/generate_data.py")
+    
+# Process the data if necessary
+if len(os.listdir(PROCESSED_DATA_FOLDER_PATH)) == 0:
+    print("Processing the data...")
+    os.system("python scripts/text_preprocessing.py")
+    
