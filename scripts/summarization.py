6bbfbe1ab61b8673fb8de5e5be110d555d94c19e
Bryce Whitney <brycewhit13@gmail.com>
2023-03-24 13:07:35 -0400

    refactor into single summarization file

--- scripts/summarization.py
+++ scripts/summarization.py
@@ -1,16 +1,14 @@
-##########################################################################################################################################
-# Filename: extractive_summarization.py                                                                                                  #
-# Author: Bryce Whitney                                                                                                                  #
-# Last Edit: 3/21/2023                                                                                                                   #
-#                                                                                                                                        #
-# Description: This script performs extractive summarization                                                                             #
-# for the rulebooks of the different sports with the TextRank                                                                            #
-# method                                                                                                                                 #
-#                                                                                                                                        #
-# References: https://github.com/AIPI540/AIPI540-Deep-Learning-Applications/blob/main/3_nlp/summarization/extractive_summarization.ipynb #
-##########################################################################################################################################
+############################################################################################################
+# Filename: summarization.py                                                                               #
+# Author: Bryce Whitney                                                                                    #
+# Last Edit: 3/24/2023                                                                                     #
+#                                                                                                          #
+# Description: This script contains methods for abstractive and extractive summarizations                  #                                                                            #
+#                                                                                                          #
+# References: https://github.com/AIPI540/AIPI540-Deep-Learning-Applications/blob/main/3_nlp/summarization/ #
+############################################################################################################
 
-# Imports
+##### Imports #####
 import os
 import numpy as np
 import networkx as nx
@@ -19,11 +17,9 @@ from nltk.cluster.util import cosine_distance
 from sklearn.feature_extraction.text import TfidfVectorizer
 from constants import PROCESSED_DATA_FOLDER_PATH
 from text_preprocessing import load_text_data, _remove_stopwords
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-#############
-# FUNCTIONS #
-#############
-
+##### Extractive Summarization #####
 def extractive_summarization(datafile, top_n=5):
     """Performs extractive summarization on the rulebooks using the TextRank method
     """
@@ -150,10 +146,79 @@ def pagerank_summarization(sentences, similarity_matrix, top_n=5):
 
     return summary
 
-#################
-# MAIN FUNCTION #
-#################
+##### Abstractive Summarization #####
 
-if __name__ == "__main__":
-    summary = extractive_summarization(datafile=os.path.join(PROCESSED_DATA_FOLDER_PATH, "ultimate_sample.txt"), top_n=3)
-    print(summary)
+def generative_summary(text_data, min_summary_length=10, max_summary_length=200):
+    """Generates a summary of the input text using the HuggingFace model sshleifer/distilbart-cnn-12-6
+
+    Args:
+        text_data (str): data to be summarized
+        min_summary_length (int, optional): the min length of the resulting summary. Defaults to 10.
+        max_summary_length (int, optional): the max length of the resulting summary. Defaults to 200.
+
+    Returns:
+        _type_: _description_
+    """
+    # Instatiate the model and tokenizer
+    model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+    tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
+    
+    # Generate and return the summary
+    summary = chunked_summary(input_text=text_data, model=model, tokenizer=tokenizer, min_chunk_len=10, max_chunk_len=200)
+    
+    print(f'Length of the source document: {len(text_data)}')
+    print(f'Length of the summary: {len(summary)}')
+    
+    return summary
+    
+def truncate_summary(input_text, model, tokenizer, min_length, max_length):
+    """Truncates the summary to the desired length
+
+    Args:
+        input_text (str): _description_
+        model (AutoModelForSeq2SeqLM): model that will generate the summary
+        tokenizer (AutoTokenizer): tokenizer for the model
+        min_length (int): min length of the summary
+        max_length (int): max length of the summary
+
+    Returns:
+        str: a truncated version of the input summary
+    """
+    inputs = tokenizer(input_text, return_tensors="pt", max_length=1024, truncation=True)
+    outputs = model.generate(inputs["input_ids"], max_length=max_length, min_length=min_length, length_penalty=1.0, num_beams=4, early_stopping=True)
+    return tokenizer.decode(outputs[0])
+
+
+def chunked_summary(input_text, model, tokenizer, min_chunk_len, max_chunk_len):
+    """Split the input text into chunks of 1024 and generate a summary for each chunk.
+    These chunks are then concatenated to form the final summary.
+
+    Args:
+        input_text (str): the input text to be summarized
+        model (AutoModelForSeq2SeqLM): model for generating the summary
+        tokenizer (AutoTokenizer): tokenizer for the model
+        min_chunk_len (int): min length of each chunk
+        max_chunk_len (int): max length of each chunk
+
+    Returns:
+        str: A summary of the input text
+    """
+    # Separate the input text into chunks
+    input_text = input_text.split(' ')
+    chunked_inputs = [input_text[i:i+1024] for i in range(0,len(input_text),1024)]
+    summary = ''
+    
+    # Get input for each chunk
+    for i, chunk in enumerate(chunked_inputs):
+        chunk = ' '.join(chunk)
+        chunk_summary = truncate_summary(input_text=chunk, model=model, tokenizer=tokenizer, min_length=min_chunk_len, max_length=max_chunk_len)
+        chunk_summary = chunk_summary.split('</s>')[-2].split('<s>')[-1].strip()
+        
+        # Add space between chunks if not the first chunk
+        if(i == 0):
+            summary += chunk_summary
+        else:
+            summary += (' '+chunk_summary)
+            
+    # Return the summary        
+    return summary
