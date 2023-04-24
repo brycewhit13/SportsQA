b26152a58077fb3e127370a49bb99dfe229b8b1a
Bryce Whitney <brycewhit13@gmail.com>
2023-04-24 19:56:16 -0400

    Update comments and remove main functionality

--- scripts/question_answering.py
+++ scripts/question_answering.py
@@ -1,7 +1,7 @@
 ##########################################################################################################################################
 # Filename: question_answering.py                                                                                                        #
 # Author: Bryce Whitney                                                                                                                  #
-# Last Edit: 4/4/2023                                                                                                                    #
+# Last Edit: 4/24/2023                                                                                                                   #
 #                                                                                                                                        #
 # Description: This script performs question and answering                                                                               #
 # for the rulebooks of the different sports with HuggingFace models                                                                      #                                                                                                                                #
@@ -9,56 +9,36 @@
 # References: https://github.com/AIPI540/AIPI540-Deep-Learning-Applications/blob/main/3_nlp/summarization/extractive_summarization.ipynb #
 ##########################################################################################################################################
 
-# Imports
+##### Imports #####
 import os
+import openai
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 from haystack import Pipeline
 from haystack.document_stores import InMemoryDocumentStore
-from haystack.nodes import BM25Retriever, FARMReader, DensePassageRetriever
+from haystack.nodes import BM25Retriever, FARMReader, DensePassageRetriever, RAGenerator
 from haystack.pipelines.standard_pipelines import TextIndexingPipeline
-from haystack.pipelines import ExtractiveQAPipeline
+from haystack.pipelines import ExtractiveQAPipeline, GenerativeQAPipeline
 
-from constants import PROCESSED_DATA_FOLDER_PATH, DOCUMENT_STORE_FOLDER_PATH
+from constants import PROCESSED_DATA_FOLDER_PATH, DOCUMENT_STORE_FOLDER_PATH, FINAL_SUMMARIES_FOLDER_PATH
 from Sport import Sport, get_league
-from data_processing import get_document_store, load_processed_data
+from data_processing import get_document_store, load_processed_data, load_summary_data
 
-# TODO: Potential problem with newlines in the text file
-# TODO: Maybe remove the article numbers in the preprocessing step
-
+#####################
 ##### FUNCTIONS #####
-def get_user_question():
-    question = input("Please enter a question: ")
-    return question
-
-def get_answer(question: str, sport: Sport):
-    # Get the file path
-    league = get_league(sport)
-    file_name = league + "_rules_processed.txt"
-    file_path = os.path.join(PROCESSED_DATA_FOLDER_PATH, file_name)
-    
-    # Initialize the retriever and reader
-    document_store = InMemoryDocumentStore(use_bm25=True)
-    retriever = BM25Retriever(document_store=document_store)
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
-    
-    # Convert the text into document objects
-    indexing_pipeline = TextIndexingPipeline(document_store)
-    indexing_pipeline.run(file_path=file_path)
-
-    # Initialize the pipeline and retrieve an answer
-    pipe = ExtractiveQAPipeline(reader, retriever)
-    answer_obj = pipe.run(query=question, params={"Retriever": {"top_k": 5}, "Reader": {"top_k": 1}})
-    
-    # Extract the answer and context from the answer object
-    answer = answer_obj["answers"][0].answer
-    context = answer_obj["answers"][0].context
-    
-    # Return the answer and the context
-    return answer, context
-
+#####################
 def query_document_store(question: str, sport: Sport, document_folder_path: str = DOCUMENT_STORE_FOLDER_PATH):
+    """Queries the document store to try and answer the user's question
+
+    Args:
+        question (str): The question the user asked
+        sport (Sport): The sport the user selected
+        document_folder_path (str, optional): The folder containing the document store databases. Defaults to DOCUMENT_STORE_FOLDER_PATH.
+
+    Returns:
+        tuple: The answer to the questions and the context from where the answer was pulled
+    """
     # Get the appropriate document store
     document_store = get_document_store(sport, document_folder_path=document_folder_path)
     
@@ -72,7 +52,7 @@ def query_document_store(question: str, sport: Sport, document_folder_path: str 
 
     # Initialize the pipeline and retrieve an answer
     pipe = ExtractiveQAPipeline(reader, retriever)
-    answer_obj = pipe.run(query=question, params={"Retriever": {"top_k": 5}, "Reader": {"top_k": 1}})
+    answer_obj = pipe.run(query=question, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 1}})
     
     # Extract the answer and context from the answer object
     answer = answer_obj["answers"][0].answer
@@ -81,6 +61,88 @@ def query_document_store(question: str, sport: Sport, document_folder_path: str 
     # Return the answer and the context
     return answer, context
 
+def gpt_answer_no_context(question: str, sport: Sport):
+    """Attemps to answer the question using GPT-3.5 without giving it any context
+
+    Args:
+        question (str): The question the user asked
+        sport (Sport): The sport the user selected
+
+    Returns:
+        str: The answer to the question
+    """
+    # Get the appropriate sport league
+    sport_league = get_league(sport)
+    
+    # Establish the prompt for the GPT-3.5 model
+    prompt = f"Following the most recent {sport_league} rules, answer the following question. \n\nQuestion: {question}"
+
+    # Make a request to GPT-3.5
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
+                                            messages=[{
+                                                "role":"user", 
+                                                "content":prompt
+                                                }],
+                                            temperature=0.1,
+                                            )
+    
+    answer = response['choices'][0]['message']['content']
+    answer = answer.replace("Answer: ", "").strip()
+    return answer
+
+def gpt_answer_with_context(question: str, sport: Sport, summary_folder: str = FINAL_SUMMARIES_FOLDER_PATH):
+    """Attempts to answer the question using GPT-3.5 while providing it a summary of the rules 
+    when the model is not confident in the answer. If it is still not confident with the added context,
+    the user will be informed in the answer. 
+
+    Args:
+        question (str): The question the user asked
+        sport (Sport): The sport the user selected
+        summary_folder (str, optional): Path to the folder containing the summaries for each sport. Defaults to FINAL_SUMMARIES_FOLDER_PATH.
+
+    Returns:
+        str: The answer to the question
+    """
+    # Get the appropriate sport league and data
+    sport_league = get_league(sport)
+    
+    # Get the gpt answer
+    gpt_answer = gpt_answer_no_context(question, sport)
+    
+    # If the model doesn't know the answer, use the summary
+    if "as an ai language model" in gpt_answer.lower():
+        summary = ""
+        with open(os.path.join(summary_folder, sport_league + "_summary_final.txt"), "r") as file:
+            summary = file.read()
+        
+        prompt = f"Following the most recent {sport_league} rules, answer the following question. \n\nQuestion: {question}"
+        
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=[
+                                                {
+                                                    "role":"user",
+                                                    "content":prompt
+                                                },
+                                                {
+                                                    "role":"assistant",
+                                                    "content":f"Here is a brief summary of the rules: {summary}"
+                                                }
+                                                ],
+                                                temperature=0.1,
+        )
+        
+        context_answer = response['choices'][0]['message']['content']
+    
+        if "as an ai language model" in context_answer.lower() or "i'm sorry" in context_answer.lower() or "i apologize" in context_answer.lower() or "summary provided" in context_answer.lower():
+            # This means neither answer is confident
+            # Remove the first sentence from the answer and tell the user we are not confident before returning
+            answer = gpt_answer[gpt_answer.find(".")+1:]
+            answer = "I am not entirely confident in my answer, so I recommend you explore the official rulebook for the answer to your question." + answer
+            return answer
+        else:
+            return context_answer.replace("Answer: ", "").strip()
+    else:
+        return gpt_answer
 
 def get_tfidf_answer(question: str, sport: Sport, processed_data_folder_path: str = PROCESSED_DATA_FOLDER_PATH):
     """Uses TF-IDF to find the sentence with the highest cosine similarity to the question and returns that sentence as the answer
@@ -108,66 +170,3 @@ def get_tfidf_answer(question: str, sport: Sport, processed_data_folder_path: st
     # Find the sentence with the highest cosine similarity
     answer = sentences[cosine_similarities.argmax()]
     return answer
-    
-##### Testing #####
-# TODO: DELETE ONCE UP AND RUNNING
-# ! FOR TESTING PURPOSES ONLY
-def _get_answer(question: str, file_path: str):
-    # Initialize the retriever and reader
-    document_store = InMemoryDocumentStore(use_bm25=True)
-    retriever = BM25Retriever(document_store=document_store)
-    reader = FARMReader(model_name_or_path="deepset/tinyroberta-squad2", use_gpu=False)
-    
-    # Convert the text into document objects
-    indexing_pipeline = TextIndexingPipeline(document_store)
-    indexing_pipeline.run(file_path=file_path)
-    
-    # Initialize the pipeline and retrieve an answer
-    pipe = ExtractiveQAPipeline(reader, retriever)
-    answer_obj = pipe.run(query=question, params={"Retriever": {"top_k": 5}, "Reader": {"top_k": 1}})
-    
-    # Extract the answer and context from the answer object
-    answer = answer_obj["answers"][0].answer
-    context = answer_obj["answers"][0].context
-    
-    return answer, context
-
-
-##### MAIN #####
-if __name__ == "__main__":   
-    sport = Sport.ULTIMATE
-    document_store_folder = os.path.join('..', DOCUMENT_STORE_FOLDER_PATH) 
-    
-    question = "How many players are on each team?"
-    answer = query_document_store(question, sport, document_folder_path="../data/document_store")
-    #answer, context = query_document_store(question, sport, document_store_folder)
-
-    print(f"Question: {question}")
-    print(f"Answer: {answer}\n")
-    
-    question2 = "What is the event organizer clause?"
-    answer2 = get_tfidf_answer(question2, sport, processed_data_folder_path="../data/processed_data")
-    #answer2, context2 = query_document_store(question, sport, document_store_folder)
-    
-    print(f"Question: {question2}")
-    print(f"Answer: {answer2}")
-    #print(f"Context: {context}")
-    '''
-    context_path = "../data/processed_data/usau_sample_rules_processed.txt"
-    
-    question = "How many players are on the field for each team?"
-    answer, context = _get_answer(question, context_path)
-    
-    question2 = "What is the event organizer clause?"
-    answer2, context2 = _get_answer(question2, context_path)
-    
-    print("\n")
-    print(f"Question: {question}")
-    print(f"Answer: {answer}")
-    print(f"Context: {context}")
-    
-    print("\n")
-    print(f"Question: {question2}")
-    print(f"Answer: {answer2}")
-    print(f"Context: {context2}")
-    '''
