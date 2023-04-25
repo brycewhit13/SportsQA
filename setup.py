b98f30710a052b9fe35f0decb80d63ec4c2dcc6a
Bryce Whitney <brycewhit13@gmail.com>
2023-04-24 20:32:20 -0400

    Update setup script

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ import sys
 sys.path.append('scripts')
 
 from scripts.constants import TEXT_DATA_FOLDER_PATH, PROCESSED_DATA_FOLDER_PATH, RAW_DATA_FOLDER_PATH, DOCUMENT_STORE_FOLDER_PATH
-from scripts.data_processing import establish_connection, download_all_data
-from scripts.Sport import Sport, get_league
 
 # Install the necessary libraries
 print("Installing the necessary libraries...")
@@ -24,23 +22,5 @@ os.system("pip install -r requirements.txt")
 print("Downloading nltk punkt and stopwords packages...")
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
-
-# Ensure all the data folders exist
-if not os.path.exists(RAW_DATA_FOLDER_PATH):
-    os.mkdir(RAW_DATA_FOLDER_PATH)
-if not os.path.exists(TEXT_DATA_FOLDER_PATH):
-    os.mkdir(TEXT_DATA_FOLDER_PATH)
-if not os.path.exists(PROCESSED_DATA_FOLDER_PATH):
-    os.mkdir(PROCESSED_DATA_FOLDER_PATH)
-    
-for sport in Sport:
-    league = get_league(sport)
-    if not os.path.exists(os.path.join(DOCUMENT_STORE_FOLDER_PATH, league)):
-        os.mkdir(os.path.join(DOCUMENT_STORE_FOLDER_PATH, league))
-
-# Download the data from azure
-print("Downloading the data from azure...")
-blob_service_client, container_client = establish_connection()
-download_all_data(container_client)
     
 print("Setup complete!")
