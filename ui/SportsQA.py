307b9dcbdc453ef0c6391d4081c598873c61eda8
Bryce Whitney <brycewhit13@gmail.com>
2024-03-24 12:56:04 -0400

    add place for response

--- ui/SportsQA.py
+++ ui/SportsQA.py
@@ -27,21 +27,19 @@ def main():
     st.title('Sports Q&A')
     
     # Create two dropdowns side by side: One to select the sport and one to select the league
-    sport = st.selectbox('Select a sport', ['🏈 Football', '🏀 Basketball', '🏒 Hockey', '🥏 Ultimate Frisbee'])
-    sport_name = ' '.join(sport.split(' ')[1:])
-    league = st.selectbox('Select a league', SPORT_LEAGUE_MAPPING[sport_name])
+    col1, col2 = st.columns(2)
+    with col1:
+        sport = st.selectbox('Select a sport', ['🏈 Football', '🏀 Basketball', '🏒 Hockey', '🥏 Ultimate Frisbee'])
+    with col2:
+        league = st.selectbox('Select a league', SPORT_LEAGUE_MAPPING[' '.join(sport.split(' ')[1:])])
     
     # Create two text boxes: One to ask a question and one where the response will be displayed
-    question_box = st.text_input('Ask a question')
-    response_box = st.empty()
+    question_box = st.text_area('Ask a question', max_chars=250)
+    response_box = st.text_area('Response', max_chars=250, disabled=True)
     
     # Create a button to submit the question
     if st.button('Submit'):
-        #response_box.text(f'You asked: {question_box}')
-        llm = load_llm_pipeline()
-        db = load_faiss_db(LEAGUE_TO_ENUM_MAPPING[league])
-        result = query_llm(llm, db, query=question_box)
-        response_box.text(result)
+        response_box.text(f'You asked: {question_box}')
         
 
 if __name__ == '__main__':
